62f2b889e4624b546085bcea754d47f40c40c343
gnogovitsin <g.nogovitsin@haulmont.com>
2021-09-25 22:31:27 +0300

    ssr

---
+++ src/client/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import {Header} from '../shared/Header';
+
+
+window.addEventListener("load", () => {
+  ReactDOM.hydrate(<Header />, document.getElementById("root"));
+})
+
