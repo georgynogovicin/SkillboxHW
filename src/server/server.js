951f0c6e0476fbf115e5dd98dd24513db2d07559
gnogovitsin <g.nogovitsin@haulmont.com>
2021-11-19 17:03:26 +0300

    fixed server & scripts for heroku

--- src/server/server.js
+++ src/server/server.js
@@ -13,7 +13,7 @@ app.use("/static", express.static("./dist/client"));
 app.get("/auth", (req, res) => {
   axios.post(
       'https://www.reddit.com/api/v1/access_token',
-      `grant_type=authorization_code&code=${req.query.code}&redirect_uri=http://localhost:3000/auth`,
+      `grant_type=authorization_code&code=${req.query.code}&redirect_uri=${process.env.HOST_NAME}:${PORT}/auth`,
       {
         auth: {username: process.env.CLIENT_ID, password: `${process.env.CLIENT_PASS}`},
         headers: {"Content-type": "application/x-www-form-urlencoded"}
