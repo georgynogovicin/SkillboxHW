7355c83e48aeb00fe4dc231d1037431bbcbecc1a
gnogovitsin <g.nogovitsin@haulmont.com>
2021-11-20 14:54:32 +0300

    Revert "fixed server & scripts for heroku"

--- src/server/server.js
+++ src/server/server.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 app.get("/auth", (req, res) => {
   axios.post(
       'https://www.reddit.com/api/v1/access_token',
-      `grant_type=authorization_code&code=${req.query.code}&redirect_uri=${process.env.HOST_NAME}:${PORT}/auth`,
+      `grant_type=authorization_code&code=${req.query.code}&redirect_uri=http://localhost:3000/auth`,
       {
         auth: {username: process.env.CLIENT_ID, password: process.env.CLIENT_PASS},
         headers: {"Content-type": "application/x-www-form-urlencoded"}
